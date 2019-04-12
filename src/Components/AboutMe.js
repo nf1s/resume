d799a133bc29931d51654887dff20c5783b4f69f
ahmednafies <ahmed.nafies@gmail.com>
2019-04-12 09:35:25 +0300

    favicon and logo added

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import ImgMediaCard from './ImageCard'
 
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing.unit * 2,
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  });
 
 class AboutMe extends Component {
-
 
     render(){
         return(
@@ -10,10 +24,11 @@ class AboutMe extends Component {
             <h1>About Me</h1>
             <p>Born and raised in Egypt</p>
             <p>Started assembling computers when I was 14 years old</p>
-            <p></p>
+            <ImgMediaCard/>
+            <ImgMediaCard/>
             </div>
         );
     }
 }
 
-export default AboutMe
+export default  withStyles(styles)(AboutMe);
