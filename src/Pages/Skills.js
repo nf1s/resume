b8b776d102b11ec3101b192503ffc1a26815297d
ahmednafies <ahmed.nafies@gmail.com>
2019-04-18 10:58:53 +0300

    progress bar cleaned

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -8,7 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import ProgressBar from '../Components/ProgressBar'
+import ProgressBar from '../Components/ProgressBar/ProgressBar'
 import Zoom from '@material-ui/core/Zoom';
 
 const styles = theme => ({
