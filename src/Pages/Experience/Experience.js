91987f6fdb358beaa4bb72775a5523b35e8c4d54
ahmednafies <ahmed.nafies@gmail.com>
2019-09-08 12:52:46 +0300

    fix work experience

--- src/Pages/Experience/Experience.js
+++ src/Pages/Experience/Experience.js
@@ -1,83 +1,119 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import { Timeline } from 'react-material-timeline';
-import { Avatar } from '@material-ui/core';
-import WorkIcon from '@material-ui/icons/WorkSharp'
-import pink from '@material-ui/core/colors/pink';
-import Slide from '@material-ui/core/Slide';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Timeline } from "react-material-timeline";
+import { Avatar } from "@material-ui/core";
+import WorkIcon from "@material-ui/icons/WorkSharp";
+import pink from "@material-ui/core/colors/pink";
+import Slide from "@material-ui/core/Slide";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   },
   grid: {
-    alignItems: 'center',
-    textAlign: 'center',
-    justify: 'center',
+    alignItems: "center",
+    textAlign: "center",
+    justify: "center"
   }
 });
 
 const events = [
   {
-    title: 'Hem Design Studio',
-    subheader: 'Dec 2018 - Now (-1 year)',
-    description: ['Full Stack Web Developer (React, Python/Django and Go)'],
-    icon: <Avatar><WorkIcon style={{ color: pink[900] }} /></Avatar>,
+    title: "Hem Design Studio",
+    subheader: "Dec 2018 - Now (-1 year)",
+    description: ["Backend Tech Lead (Python/Django, Go)"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[900] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'Swedbank',
-    subheader: 'Jan 2017 - Dec 2018 (2 Years)',
-    description: ['Full Stack Web Developer (AngularJs, Jquery, Python/Flask)'],
-    icon: <Avatar><WorkIcon style={{ color: pink[800] }} /></Avatar>,
+    title: "Swedbank",
+    subheader: "Jan 2017 - Dec 2018 (2 Years)",
+    description: ["Software Developer (Python/Flask, AngularJs, Jquery)"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[800] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'Erasmus Student Network',
-    subheader: 'Jan 2016 - Dec 2016 (1 Year)',
-    description: ['Web Developer'],
-    icon: <Avatar><WorkIcon style={{ color: pink[700] }} /></Avatar>,
+    title: "Erasmus Student Network",
+    subheader: "Jan 2016 - Dec 2016 (1 Year)",
+    description: ["Software Developer (Python/Django, Jquery)"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[700] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'Skype',
-    subheader: 'Oct 2016 - Dec 2016 (3 months)',
-    description: ['Community Moderator'],
-    icon: <Avatar><WorkIcon style={{ color: pink[600] }} /></Avatar>,
+    title: "Skype",
+    subheader: "Oct 2016 - Dec 2016 (3 months)",
+    description: ["Community Moderator"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[600] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'Praxis Network',
-    subheader: 'Jul 2015 - Sep 2015 (3 months)',
-    description: ['Intern'],
-    icon: <Avatar><WorkIcon style={{ color: pink[500] }} /></Avatar>,
+    title: "Praxis Network",
+    subheader: "Jul 2015 - Sep 2015 (3 months)",
+    description: ["Web developer Intern (PHP/Symphony)"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[500] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'YBN LTD',
-    subheader: 'Jan 2014 - Jan 2015 (1 Year)',
-    description: ['Web Developer Consultant'],
-    icon: <Avatar><WorkIcon style={{ color: pink[400] }} /></Avatar>,
+    title: "YBN LTD",
+    subheader: "Jan 2014 - Jan 2015 (1 Year)",
+    description: ["Web Developer Consultant (Python/Django, Java/Spring)"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[400] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'British University in Egypt',
-    subheader: 'Jan 2014 - Jan 2015 (1 Year)',
-    description: ['Teaching Assistant'],
-    icon: <Avatar><WorkIcon style={{ color: pink[300] }} /></Avatar>,
+    title: "British University in Egypt",
+    subheader: "Jan 2014 - Jan 2015 (1 Year)",
+    description: ["Teaching Assistant (c/c++, Java)"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[300] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'Motorolla',
-    subheader: 'Aug 2011 - Sep 2011 (2 months)',
-    description: ['Intern'],
-    icon: <Avatar><WorkIcon style={{ color: pink[200] }} /></Avatar>,
+    title: "Motorolla",
+    subheader: "Aug 2011 - Sep 2011 (2 months)",
+    description: ["Intern"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[200] }} />
+      </Avatar>
+    )
   },
   {
-    title: 'Orange',
-    subheader: 'Aug 2010 - Sep 2010(2 months)',
-    description: ['Intern'],
-    icon: <Avatar><WorkIcon style={{ color: pink[100] }} /></Avatar>,
-  },
+    title: "Orange",
+    subheader: "Aug 2010 - Sep 2010(2 months)",
+    description: ["Intern"],
+    icon: (
+      <Avatar>
+        <WorkIcon style={{ color: pink[100] }} />
+      </Avatar>
+    )
+  }
 ];
 
 function Experience(props) {
@@ -89,7 +125,13 @@ function Experience(props) {
         <Grid item xs={12} sm={12} md={12} lg={8} className={classes.grid}>
           <h1>Work Experience</h1>
         </Grid>
-        <Slide direction="up" in={true} style={{ transitionDelay: 100 }} mountOnEnter unmountOnExit>
+        <Slide
+          direction="up"
+          in={true}
+          style={{ transitionDelay: 100 }}
+          mountOnEnter
+          unmountOnExit
+        >
           <Grid item xs={12} sm={12} md={12} lg={8}>
             <Timeline events={events} />
           </Grid>
