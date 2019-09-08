b964e569ffa90d6a56338add8a1ae6cb31875a78
ahmednafies <ahmed.nafies@gmail.com>
2019-09-08 12:45:57 +0300

    fixes unused imports and line breaks

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,58 +1,68 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import List from "@material-ui/core/List";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Divider from '@material-ui/core/Divider';
-import Avatar from '@material-ui/core/Avatar';
-import ContactEmailIcon from '@material-ui/icons/ContactMailSharp'
-import pink from '@material-ui/core/colors/pink';
-import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Divider from "@material-ui/core/Divider";
+import Avatar from "@material-ui/core/Avatar";
+import ContactEmailIcon from "@material-ui/icons/ContactMailSharp";
+import pink from "@material-ui/core/colors/pink";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   },
   grid: {
-    alignItems: 'center',
-    textAlign: 'center',
-    justify: 'center',
+    alignItems: "center",
+    textAlign: "center",
+    justify: "center"
   }
 });
-const listItem = (text, icon, ) => {
-  return <span>
-    <Divider />
-    <ListItem>
-      <ListItemIcon>
-        <Avatar>{icon}</Avatar>
-      </ListItemIcon>
-      <ListItemText primary={text} />
-    </ListItem>
-  </span>
+const listItem = (text, icon) => {
+  return (
+    <span>
+      <Divider />
+      <ListItem>
+        <ListItemIcon>
+          <Avatar>{icon}</Avatar>
+        </ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItem>
+    </span>
+  );
 };
-
 
 const listItems = () => {
-  return <List>
-    {listItem("ahmed.nafies@gmail.com", <ContactEmailIcon style={{ color: pink[900] }} />)}
-    {listItem("https://github.com/ahmednafies", <FaGithubSquare style={{ color: pink[900] }} />)}
-    {listItem("https://www.linkedin.com/in/ahmednafies", <FaLinkedin style={{ color: pink[900] }} />)}
-  </List>
+  return (
+    <List>
+      {listItem(
+        "ahmed.nafies@gmail.com",
+        <ContactEmailIcon style={{ color: pink[900] }} />
+      )}
+      {listItem(
+        "https://github.com/ahmednafies",
+        <FaGithubSquare style={{ color: pink[900] }} />
+      )}
+      {listItem(
+        "https://www.linkedin.com/in/ahmednafies",
+        <FaLinkedin style={{ color: pink[900] }} />
+      )}
+    </List>
+  );
 };
 
-
 function Contact(props) {
-  const { classes } = props
+  const { classes } = props;
   return (
     <div className={classes.root}>
       <Grid container spacing={24}>
@@ -66,7 +76,7 @@ function Contact(props) {
 }
 
 Contact.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Contact);
--- src/Pages/Impact/Impact.js
+++ src/Pages/Impact/Impact.js
@@ -1,68 +1,66 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Graph from '../../Components/Graph';
-import Slide from '@material-ui/core/Slide';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Graph from "../../Components/Graph";
+import Slide from "@material-ui/core/Slide";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   },
   grid: {
-    alignItems: 'center',
-    textAlign: 'center',
-    justify: 'center',
+    alignItems: "center",
+    textAlign: "center",
+    justify: "center"
   }
 });
 
 const impacts = [
   {
     project: "Swedbank Internal Cloud",
-    description: "Automating virtual machine ordering,\
-        the most complex project I have ever worked in, \
-        we took machine ordering and derlivery from an Average of 3 weeks to just 15 minutes",
+    description:
+      "Automating virtual machine ordering, the most complex project I have ever worked in, we took machine ordering and derlivery from an Average of 3 weeks to just 15 minutes",
     type: "time_in_minutes",
     data: [
-      { name: 'Mar', time_in_minutes: 30240 },
-      { name: 'Jun', time_in_minutes: 1440 },
-      { name: 'Sep', time_in_minutes: 180 },
-      { name: 'Dec', time_in_minutes: 15 },
+      { name: "Mar", time_in_minutes: 30240 },
+      { name: "Jun", time_in_minutes: 1440 },
+      { name: "Sep", time_in_minutes: 180 },
+      { name: "Dec", time_in_minutes: 15 }
     ]
   },
   {
     project: "Praxis Student Network",
-    description: "Creating a Platform where Students can find Erasmus plus internships, \
-      This Platform has helped atleast 20,000 students find internships.",
+    description:
+      "Creating a Platform where Students can find Erasmus plus internships, This Platform has helped atleast 20,000 students find internships.",
     type: "students",
     data: [
-      { name: 'Jun', students: 0 },
-      { name: 'Jul', students: 5000 },
-      { name: 'Sep', students: 10000 },
-      { name: 'Oct', students: 20000 },
+      { name: "Jun", students: 0 },
+      { name: "Jul", students: 5000 },
+      { name: "Sep", students: 10000 },
+      { name: "Oct", students: 20000 }
     ]
   },
 
   {
     project: "ESN Buddy System",
-    description: "System which matches buddies to incoming international students,\
-       Impacts around 2000 students coming to Tallinn every year ",
+    description:
+      "System which matches buddies to incoming international students, Impacts around 2000 students coming to Tallinn every year ",
     type: "students",
     data: [
-      { name: 'Sep', students: 0 },
-      { name: 'Oct', students: 500 },
-      { name: 'Nov', students: 1000 },
-      { name: 'Dec', students: 2000 },
+      { name: "Sep", students: 0 },
+      { name: "Oct", students: 500 },
+      { name: "Nov", students: 1000 },
+      { name: "Dec", students: 2000 }
     ]
-  },
+  }
 ];
 
 function Impact(props) {
-
   const { classes } = props;
 
   return (
@@ -71,12 +69,20 @@ function Impact(props) {
       <div className={classes.root}>
         <Grid container spacing={24}>
           {impacts.map((impact, index) => {
-            return <Slide direction="left" in={true} key={index}
-              style={{ transitionDelay: index * 50 }} mountOnEnter unmountOnExit>
-              <Grid item xs={8} className={classes.grid}>
-                <Graph impact={impact} />
-              </Grid>
-            </Slide>
+            return (
+              <Slide
+                direction="left"
+                in={true}
+                key={index}
+                style={{ transitionDelay: index * 50 }}
+                mountOnEnter
+                unmountOnExit
+              >
+                <Grid item xs={8} className={classes.grid}>
+                  <Graph impact={impact} />
+                </Grid>
+              </Slide>
+            );
           })}
         </Grid>
       </div>
@@ -84,4 +90,4 @@ function Impact(props) {
   );
 }
 
-export default withStyles(styles)(Impact)
+export default withStyles(styles)(Impact);
