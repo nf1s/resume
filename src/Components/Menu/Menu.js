4e4181843897ed8dbf90c5c28be453167d8e446f
ahmednafies <ahmed.nafies@gmail.com>
2020-05-03 11:16:11 +0200

    fixes menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,42 +1,44 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {withStyles} from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import AccountIcon from '@material-ui/icons/AccountCircleTwoTone';
-import SchoolIcon from '@material-ui/icons/SchoolTwoTone';
-import TrendingUpIcon from '@material-ui/icons/TrendingUpTwoTone';
-import TimelineIcon from '@material-ui/icons/TimelineTwoTone';
-import CodeIcon from '@material-ui/icons/CodeTwoTone';
-import LocalLibraryIcon from '@material-ui/icons/LocalLibraryTwoTone';
-import {Link} from 'react-router-dom';
-import pink from '@material-ui/core/colors/pink';
-import ContactsIcon from '@material-ui/icons/ContactsTwoTone';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import AccountIcon from "@material-ui/icons/AccountCircleTwoTone";
+import SchoolIcon from "@material-ui/icons/SchoolTwoTone";
+import TrendingUpIcon from "@material-ui/icons/TrendingUpTwoTone";
+import TimelineIcon from "@material-ui/icons/TimelineTwoTone";
+import CodeIcon from "@material-ui/icons/CodeTwoTone";
+import LocalLibraryIcon from "@material-ui/icons/LocalLibraryTwoTone";
+import { Link } from "react-router-dom";
+import pink from "@material-ui/core/colors/pink";
+import ContactsIcon from "@material-ui/icons/ContactsTwoTone";
+import ImportContactsTwoToneIcon from "@material-ui/icons/ImportContactsTwoTone";
+import BuildIcon from "@material-ui/icons/BuildTwoTone";
 
 const drawerWidth = 240;
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   menuBackground: {
-    color: '#fff',
+    color: "#fff",
     backgroundColor: pink[900],
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
@@ -44,7 +46,7 @@ const styles = theme => ({
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -54,7 +56,7 @@ const styles = theme => ({
     marginRight: 20,
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
@@ -64,23 +66,23 @@ const styles = theme => ({
     width: drawerWidth,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 8px',
+    display: "flex",
+    alignItems: "center",
+    padding: "0 8px",
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -88,64 +90,62 @@ const styles = theme => ({
   },
 });
 
-const menuItem = (name, icon, path) => {
-  return (
-    <Link to={'/' + path} style={{textDecoration: 'none'}}>
-      <MenuItem>
-        <ListItemIcon>{icon}</ListItemIcon>
-        {name}
-      </MenuItem>
-    </Link>
-  );
-};
-
-const getMenuItems = () => {
-  return (
-    <MenuList>
-      {menuItem(
-        'About Me',
-        <AccountIcon style={{color: pink[600]}} />,
-        'about',
-      )}
-      {menuItem(
-        'Education',
-        <SchoolIcon style={{color: pink[600]}} />,
-        'education',
-      )}
-      {menuItem(
-        'Experience',
-        <TimelineIcon style={{color: pink[600]}} />,
-        'experience',
-      )}
-      {menuItem('Skills', <CodeIcon style={{color: pink[600]}} />, 'skills')}
-      {menuItem(
-        'Impact',
-        <TrendingUpIcon style={{color: pink[600]}} />,
-        'impact',
-      )}
-      {menuItem(
-        'Books',
-        <LocalLibraryIcon style={{color: pink[600]}} />,
-        'books',
-      )}
-      {menuItem(
-        'Contact',
-        <ContactsIcon style={{color: pink[600]}} />,
-        'contact',
-      )}
-      {menuItem(
-        'Articles',
-        <ContactsIcon style={{color: pink[600]}} />,
-        'articles',
-      )}
-      {menuItem(
-        'Projects',
-        <ContactsIcon style={{color: pink[600]}} />,
-        'projects',
-      )}
-    </MenuList>
-  );
-};
+const menuItems = [
+  {
+    id: 1,
+    title: "About Me",
+    icon: <AccountIcon style={{ color: pink[600] }} />,
+    path: "about",
+  },
+  {
+    id: 2,
+    title: "Education",
+    icon: <SchoolIcon style={{ color: pink[600] }} />,
+    path: "education",
+  },
+  {
+    id: 3,
+    title: "Experience",
+    icon: <TimelineIcon style={{ color: pink[600] }} />,
+    path: "experience",
+  },
+  {
+    id: 4,
+    title: "Skills",
+    icon: <CodeIcon style={{ color: pink[600] }} />,
+    path: "skills",
+  },
+  {
+    id: 5,
+    title: "Impact",
+    icon: <TrendingUpIcon style={{ color: pink[600] }} />,
+    path: "impact",
+  },
+  {
+    id: 6,
+    title: "Books",
+    icon: <LocalLibraryIcon style={{ color: pink[600] }} />,
+    path: "books",
+  },
+  {
+    id: 7,
+    title: "Articles",
+    icon: <ImportContactsTwoToneIcon style={{ color: pink[600] }} />,
+    path: "articles",
+  },
+  {
+    id: 8,
+    title: "Projects",
+    icon: <BuildIcon style={{ color: pink[600] }} />,
+    path: "projects",
+  },
+  {
+    id: 9,
+    title: "Contact",
+    icon: <ContactsIcon style={{ color: pink[600] }} />,
+    path: "contact",
+  },
+];
 
 function PersistentDrawerLeft(props) {
   const [open, setOpen] = useState(false);
@@ -158,7 +158,7 @@ function PersistentDrawerLeft(props) {
     setOpen(false);
   };
 
-  const {classes, theme, children} = props;
+  const { classes, theme, children } = props;
 
   return (
     <div className={classes.root}>
@@ -167,13 +167,15 @@ function PersistentDrawerLeft(props) {
         position="fixed"
         className={classNames(classes.appBar, classes.menuBackground, {
           [classes.appBarShift]: open,
-        })}>
+        })}
+      >
         <Toolbar disableGutters={!open}>
           <IconButton
             color="inherit"
             aria-label="Open drawer"
             onClick={handleDrawerOpen}
-            className={classNames(classes.menuButton, open && classes.hide)}>
+            className={classNames(classes.menuButton, open && classes.hide)}
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" noWrap>
@@ -188,10 +190,11 @@ function PersistentDrawerLeft(props) {
         open={open}
         classes={{
           paper: classes.drawerPaper,
-        }}>
+        }}
+      >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? (
+            {theme.direction === "ltr" ? (
               <ChevronLeftIcon />
             ) : (
               <ChevronRightIcon />
@@ -199,13 +202,29 @@ function PersistentDrawerLeft(props) {
           </IconButton>
         </div>
         <Divider />
-        {getMenuItems()}
+        <MenuList>
+          {menuItems.map((item) => {
+            return (
+              <Link
+                key={item.id}
+                to={"/" + item.path}
+                style={{ textDecoration: "none" }}
+              >
+                <MenuItem>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  {item.title}
+                </MenuItem>
+              </Link>
+            );
+          })}
+        </MenuList>
       </Drawer>
 
       <main
         className={classNames(classes.content, {
           [classes.contentShift]: open,
-        })}>
+        })}
+      >
         <div className={classes.drawerHeader} />
         <div>{children}</div>
       </main>
@@ -219,4 +238,4 @@ PersistentDrawerLeft.propTypes = {
   children: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles, {withTheme: true})(PersistentDrawerLeft);
+export default withStyles(styles, { withTheme: true })(PersistentDrawerLeft);
