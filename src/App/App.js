f6fd0970058992dce9ef3bcf59cafba718244015
ahmednafies <ahmed.nafies@gmail.com>
2020-05-03 16:12:35 +0200

    fixes aoo

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import Menu from '../Components/Menu/Menu';
-import './App.css';
-import {BrowserRouter} from 'react-router-dom';
-import Routes from '../Routes';
+import React from "react";
+import Menu from "../Components/Menu/Menu";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "../Routes";
 
 function App() {
   return (
