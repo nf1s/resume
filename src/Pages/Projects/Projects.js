da2bf406daa2003fb6f22f80844dfda9122b44c5
ahmednafies <ahmed.nafies@gmail.com>
2020-05-03 11:16:32 +0200

    fixes

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Zoom from '@material-ui/core/Zoom';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Zoom from "@material-ui/core/Zoom";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
+    overflowX: "auto",
   },
   table: {
     minWidth: 10,
@@ -24,23 +24,23 @@ const styles = theme => ({
 let id = 0;
 function createData(name, description) {
   id += 1;
-  return {id, name, description};
+  return { id, name, description };
 }
 
 const rows = [
   createData(
-    'Covid',
-    'Python package to get covid info from John Hopkins university API and Worldometers.info',
+    "Covid",
+    "Python package to get covid info from John Hopkins university API and Worldometers.info"
   ),
 ];
 
 function Projects(props) {
-  const {classes} = props;
+  const { classes } = props;
 
   return (
     <Grid container spacing={24}>
       <Grid item xs={8}>
-        <Zoom in={true} style={{transitionDelay: 20}}>
+        <Zoom in={true} style={{ transitionDelay: 20 }}>
           <Paper className={classes.root}>
             <Table className={classes.table}>
               <TableHead>
@@ -50,7 +50,7 @@ function Projects(props) {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map(row => (
+                {rows.map((row) => (
                   <TableRow key={row.id}>
                     <TableCell component="th" scope="row">
                       <b>{row.name}</b>
